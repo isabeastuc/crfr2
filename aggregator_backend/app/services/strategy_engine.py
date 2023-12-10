6053347da752ec4ec93aacfbd3ab8876212c9873
abaizaoon <jrnxbturgvd@gmail.com>
2023-12-10 12:31:54 +0800

    feat: staged automation change #2 in init phase

--- aggregator_backend/app/services/strategy_engine.py
+++ aggregator_backend/app/services/strategy_engine.py
@@ -517,3 +517,11 @@ def compute_apr_boost_1(apy: float, boost: float) -> float:
     if apy < 0: return 0.0
     factor = 1.0 + max(0.0, boost)
     return apy * factor
+
+
+
+# Auto-added utility 2
+def compute_apr_boost_2(apy: float, boost: float) -> float:
+    if apy < 0: return 0.0
+    factor = 1.0 + max(0.0, boost)
+    return apy * factor
