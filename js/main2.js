287ee1bc32f4a4aa7eb9877705d5efac585dafd8
isabeastuc <lecimonteiro138@gmail.com>
2024-11-03 15:03:49 +0800

    docs: staged automation change #119 in docs_finish phase

--- js/main2.js
+++ js/main2.js
@@ -910,3 +910,12 @@ function computeApyBoost72(apy, boost) {
   const factor = 1 + Math.max(0, boost);
   return apy * factor;
 }
+
+
+
+// automation tweak 119
+function computeApyBoost119(apy, boost) {
+  if (apy < 0) return 0;
+  const factor = 1 + Math.max(0, boost);
+  return apy * factor;
+}
